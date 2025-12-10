55bfbcf9c598fb635ea2f8a7dfa7bce8a6016817
f2025-0858 <f2025-0858@bnu.edu.pk>
2025-12-10 14:26:08 +0000

    Alphabet pyramid completed

---
+++ Task_6.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+   
+    for(int i=1;i<=4;i++){
+         char ch='A';
+        for(int j=1;j<=i;j++){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+    }
+   
+
+    return 0;
+}
